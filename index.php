d6c43208dc61805b4bb2033035bfd3278ed230f1
Unknown <martinuccimatias@gmail.com>
2019-09-11 21:35:09 -0300

    Creo header
    y lo utilizo en el index

--- index.php
+++ index.php
@@ -1,42 +1,6 @@
-<?php
-
-  
-
-?>
-
-
-
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/index.css">
-    <title>DigitalMe</title>
-</head>
-<body>
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="index.php"> <img class="brand-logo" src="images/dm-logo.svg" alt=""> </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-center" id="navbarTogglerDemo01">
-        <div class="navbar-nav">
-          <a class="nav-item nav-link" href="index.php">Descubrir</a>
-          <a class="nav-item nav-link" href="index.php">En directo</a>
-          <a class="nav-item nav-link" href="faq.php">Ayuda</a>
-        </div>
-        <div class="d-flex flex-row">
-          <button type="button" class="btn btn-primary"><a class="iniciar" href="login.php">Iniciar sesión con correo</a></button>
-          <p class="o">o</p>
-          <a href="www.google.com"><img class="logos" src="images/busqueda.png" alt=""></a>
-          <a href="www.facebook.com"><img class="logos" src="images/facebook.png" alt=""></a>
-          <a class="lupa" href="www.google.com"><ion-icon name="search"></ion-icon></a>
-        </div>
-</div>
-</nav>
+<?php include('header.php');?>
+
 <section>
 <div class="descripcion">
 <h2 class="date">Date a conocer.</h2>
