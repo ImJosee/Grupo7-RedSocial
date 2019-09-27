923d72703fe3dcbae45ea6c9799d64c60ebe6489
MatiasMartinu <matias@xintel.com.ar>
2019-09-27 11:34:05 -0300

    Unifico estilos login

--- login.php
+++ login.php
@@ -33,13 +33,12 @@ if($_POST) {
 	} 
 }
 
-
 include('header.php');
 ?>
     <div class="container-fluid">
       <div class="bloque-inicio col-sm-12 col-md-10 col-lg-6 col-xl-4">
-        <h2 class="inicia">Inicia  sesíon para continuar</h2>
-        <h1>DigitalMe</h1>
+        <h2 class="inicia h2Login">Inicia  sesíon para continuar</h2>
+        <h1 class="h1Login">DigitalMe</h1>
         <form action="login.php" method="post">
           <div class="form-group row">
             <div class="col-12">
@@ -57,14 +56,14 @@ include('header.php');
             <label class="form-check-label" for="exampleCheck1">Mantener sesión iniciada</label>
             <a class="" href="index.php">¿Contraseña olvidada?</a>
           </div>
-          <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+          <button type="submit" class="btn btn-primary btn-login">Iniciar sesión</button>
         </form>
         <h3 class="miembro">¿Todavía no eres miembro? </h3><a class="registrarse" href="register.php">Registrarse</a>
         <h3 class="empresa">¿Deseas utilizar la cuenta de tu empresa o de tu centro educativo?</h3>
         <a href="register.php">Iniciar sesión con un Enterprise ID</a>
         <h3 class="empresa">O inicia sesión con</h3>
-        <a class="google" href="www.google.com"><img class="logos" src="images/busqueda.png" alt=""></a>
-        <a class="facebook" href="www.facebook.com"><img class="logos" src="images/facebook.png" alt=""></a>
+        <a class="google" href="www.google.com"><img class="logos-login" src="images/busqueda.png" alt=""></a>
+        <a class="facebook" href="www.facebook.com"><img class="logos-login" src="images/facebook.png" alt=""></a>
         </div>
       </div>
 <?php include('footer.php');?>
